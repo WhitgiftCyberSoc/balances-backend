126411668b72eb75c84520fad8a505befb5f210e
Kelvin <hello@iamkelv.in>
2015-02-10 19:18:07 +0000

    Remove sample code; add initial validation

--- app/Http/Controllers/WisePayController.php
+++ app/Http/Controllers/WisePayController.php
@@ -11,7 +11,21 @@ class WisePayController extends Controller {
 
     public function check(Request $request){
 
-        //
+        // Validate HTTPS connection
+        if (!(Request::secure())) {
+            return Response::json(['error' => 'true', 'message' => 'The server has rejected an insecure connection.'], 400);
+        }
+
+        // Validate Content-Type
+        if ($request->header('Content-Type') != 'application/x-www-form-urlencoded') {
+            return Response::json(['error' => 'true', 'message' => 'There has been an issue handling the request.'], 400);
+        }
+
+        // TODO: Authenticate user
+
+        // TODO: Scrape balances with error checking
+
+        // TODO: Return balances
 
 	}
 
