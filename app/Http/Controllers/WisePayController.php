10ac2ed00e6ad411b2959b8a813c323eb99b66dc
Kelvin <hello@iamkelv.in>
2015-02-11 19:43:49 +0000

    Add validation of credentials to WisePayController.php; remove CSRF checking from Kernel.php

--- app/Http/Controllers/WisePayController.php
+++ app/Http/Controllers/WisePayController.php
@@ -1,9 +1,7 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Requests;
+use App\Http\Request;
 use App\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
 
 use GuzzleHttp\Client;
 
@@ -16,12 +14,30 @@ class WisePayController extends Controller {
             return Response::json(['error' => 'true', 'message' => 'The server has rejected an insecure connection.'], 400);
         }
 
-        // Validate Content-Type
-        if ($request->header('Content-Type') != 'application/x-www-form-urlencoded') {
-            return Response::json(['error' => 'true', 'message' => 'There has been an issue handling the request.'], 400);
+        // Validate request method
+        if (!(Request::isMethod('post'))) {
+            return Response::json(['error' => 'true', 'message' => 'The server has rejected an improper request method.'], 400);
         }
 
-        // TODO: Authenticate user
+        // Validate Content-Type
+        if ($request->header('Content-Type') != 'application/x-www-form-urlencoded') {
+            return Response::json(['error' => 'true', 'message' => 'The server has rejected an improper request header.'], 400);
+        }
+
+        // Retrieve and validate credentials from request
+        $username = Request::input('username');
+        $password = Request::input('password');
+
+        if (empty($username)) {
+            return Response::json(['error' => 'true', 'message' => 'The username or password is missing.'], 401);
+        }
+
+        if (empty($password)) {
+            return Response::json(['error' => 'true', 'message' => 'The username or password is missing.'], 401);
+        }
+
+        // Authenticate user
+        $client = new GuzzleHttp\Client();
 
         // TODO: Scrape balances with error checking
 
