ff29815e0bb1bcbce8d0370e077d5c1ef6191704
Kelvin <hello@iamkelv.in>
2015-02-11 19:58:08 +0000

    Fix namespaces and methods in WisePayController.php

--- app/Http/Controllers/WisePayController.php
+++ app/Http/Controllers/WisePayController.php
@@ -1,7 +1,10 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Request;
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 use GuzzleHttp\Client;
 
@@ -10,30 +13,30 @@ class WisePayController extends Controller {
     public function check(Request $request){
 
         // Validate HTTPS connection
-        if (!(Request::secure())) {
-            return Response::json(['error' => 'true', 'message' => 'The server has rejected an insecure connection.'], 400);
+        if (!($request->secure())) {
+            return response()->json(['error' => 'true', 'message' => 'The server has rejected an insecure connection.'], 400);
         }
 
         // Validate request method
-        if (!(Request::isMethod('post'))) {
-            return Response::json(['error' => 'true', 'message' => 'The server has rejected an improper request method.'], 400);
+        if (!($request->isMethod('post'))) {
+            return response()->json(['error' => 'true', 'message' => 'The server has rejected an improper request method.'], 400);
         }
 
         // Validate Content-Type
         if ($request->header('Content-Type') != 'application/x-www-form-urlencoded') {
-            return Response::json(['error' => 'true', 'message' => 'The server has rejected an improper request header.'], 400);
+            return response()->json(['error' => 'true', 'message' => 'The server has rejected an improper request header.'], 400);
         }
 
         // Retrieve and validate credentials from request
-        $username = Request::input('username');
-        $password = Request::input('password');
+        $username = $request->input('username');
+        $password = $request->input('password');
 
         if (empty($username)) {
-            return Response::json(['error' => 'true', 'message' => 'The username or password is missing.'], 401);
+            return response()->json(['error' => 'true', 'message' => 'The username or password is missing.'], 401);
         }
 
         if (empty($password)) {
-            return Response::json(['error' => 'true', 'message' => 'The username or password is missing.'], 401);
+            return response()->json(['error' => 'true', 'message' => 'The username or password is missing.'], 401);
         }
 
         // Authenticate user
