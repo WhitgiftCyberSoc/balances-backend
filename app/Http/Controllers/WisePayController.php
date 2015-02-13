95e22d3e88a6e4471c3cee1ce15f3520c1d85dfa
Kelvin <hello@iamkelv.in>
2015-02-13 21:24:58 +0000

    Modify WisePayController.php; add stil/xpath-selector

--- app/Http/Controllers/WisePayController.php
+++ app/Http/Controllers/WisePayController.php
@@ -10,6 +10,9 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJarInterface;
 use GuzzleHttp\Cookie\CookieJar;
 
+use XPathSelector\Selector;
+use XPathSelector\Exception\NodeNotFoundException;
+
 /**
  * Class WisePayController
  * @package App\Http\Controllers
@@ -31,24 +34,75 @@ class WisePayController extends Controller
 
     public function check()
     {
-        // TODO: Return balances
-        $balanceRequest = $this->scrapeBalances();
+        $scrapeRequest = $this->scrapeBalances();
 
         // Pass on previous errors
-        if ($balanceRequest['error'] == 'true') {
-            return $balanceRequest['response'];
+        if ($scrapeRequest['error'] == 'true') {
+            return $scrapeRequest['response'];
         }
+
+        return response()->json(['balances' => $scrapeRequest['balances']], 200);
     }
 
     public function scrapeBalances()
     {
-        // TODO: Scrape balances and check for errors
+        $client = new Client();
         $authRequest = $this->authUser();
 
         // Pass on previous errors
         if ($authRequest['error'] == 'true') {
             return $authRequest;
         }
+
+        // Scrape balances from WisePay website
+        $cookieJar = $authRequest['cookie'];
+        $response = $client->get('https://www.wisepay.co.uk/store/parent/default.asp?view=account', [
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:27.0) Gecko/20100101 Waterfox/27.0 Firefox/27.0.1',
+                'Host' => 'www.wisepay.co.uk'
+            ],
+            'cookies' => $cookieJar
+        ]);
+
+        // Check for non-200 HTTP status code
+        if ($response->getStatusCode() != 200) {
+            return [
+                'error' => 'true',
+                'response' => response()->json(['error' => 'true', 'message' => 'The WisePay server returned a non-200 status code - ' . $response->getStatusCode()], 502)
+            ];
+        }
+
+        // Load HTML into DOM
+        $code = $response->getBody();
+        $xs = Selector::loadHTML($code);
+
+        // Scrape balances from DOM
+        try {
+            $balances['lunch'] = $xs->find('//*[@id="page_content"]/table[2]/tbody/tr[2]/td/table[3]/tbody/tr/td/b[1]');
+            $balances['tuck'] = $xs->find('//*[@id="page_content"]/table[2]/tbody/tr[2]/td/table[3]/tbody/tr/td/b[2]');
+        } catch (NodeNotFoundException $e) {
+            return [
+                'error' => 'true',
+                'response' => response()->json(['error' => 'true', 'message' => 'The server has been unable to obtain the balances.'], 500)
+            ];
+        }
+
+        // Extract double from balances
+        foreach ($balances as &$balance) {
+            if (preg_match('/((\d+)\.(\d+))/', $balance, $match)) {
+                $balance = floatval($match);
+            } else {
+                return [
+                    'error' => 'true',
+                    'response' => response()->json(['error' => 'true', 'message' => 'The server has been unable to obtain and match the balances.'], 500)
+                ];
+            }
+        }
+
+        return [
+            'error' => 'false',
+            'balances' => $balances
+        ];
     }
 
     public function authUser()
@@ -56,7 +110,7 @@ class WisePayController extends Controller
         $client = new Client();
         $cookieJar = new CookieJar();
 
-        // TODO: Authenticate user with WisePay website
+        // Authenticate user with WisePay website
         $response = $client->post('https://www.wisepay.co.uk/store/parent/process.asp', [
             'headers' => [
                 'User-Agent' => 'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:27.0) Gecko/20100101 Waterfox/27.0 Firefox/27.0.1',
@@ -70,7 +124,6 @@ class WisePayController extends Controller
                 'acc_password' => $this->request->input('password'),
                 'x' => '0',
                 'y' => '0',
-                'timeout' => '5'
             ],
             'cookies' => $cookieJar
         ]);
